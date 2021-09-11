5408f6788399e3e1401f4b99a2a4831253eef927
Nathan Silva <nathan.osilva1999@hotmail.com>
2021-09-11 20:22:44 -0300

    Ajustando conexao com o banco

---
+++ Ligarbase/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Ligarbase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLigar = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clique no botão para ligar a base";
+            // 
+            // btnLigar
+            // 
+            this.btnLigar.Location = new System.Drawing.Point(12, 42);
+            this.btnLigar.Name = "btnLigar";
+            this.btnLigar.Size = new System.Drawing.Size(180, 23);
+            this.btnLigar.TabIndex = 1;
+            this.btnLigar.Text = "Ligar";
+            this.btnLigar.UseVisualStyleBackColor = true;
+            this.btnLigar.Click += new System.EventHandler(this.btnLigar_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 83);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(38, 15);
+            this.lbStatus.TabIndex = 2;
+            this.lbStatus.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 122);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnLigar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ligar Base";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLigar;
+        private System.Windows.Forms.Label lbStatus;
+    }
+}
+
---
+++ Ligarbase/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using DAL;
+
+namespace Ligarbase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            lbStatus.Text = "";
+        }
+
+        private void btnLigar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conec = null;
+
+            try
+            {
+                conec = new SqlConnection(Common.get());
+                SqlCommand cmd = new SqlCommand("",conec);
+
+                //define instrução SQL - nome de tabela/armazenamento/etc
+                cmd.CommandText = @"CREATE DATABASE vrsolucoes ON" +
+                    "(FILENAME = 'C:\\VRControleSolucoes\\vrsolucoes.mdf')" +
+                    "(FILENAME = 'C:\\VRControleSolucoes\\vrcoluoes_log.ldf')" +
+                    "FOR ATTACH";
+
+                conec.Open();
+               
+                lbStatus.Text = "Sucesso";
+
+            }
+            catch
+            {
+                lbStatus.Text = "Erro";
+            }
+        }
+    }
+}
